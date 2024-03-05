78cf1ff634c2cd03ca5282a875720cc018c5470a
Antar Das <antardas2334@gmail.com>
2024-03-05 19:34:21 +0600

    Added Unit Test for Register

--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -3,9 +3,18 @@ import { HttpResponse, http } from 'msw';
 import { existingUser, userJwt } from '~/mocks/data/user.mock';
 const BASE_URL: string = `http://localhost:5000/api/v1`;
 // {  _request, _params, _cookies    }
-export const signupMock = http.post(`${BASE_URL}/singup`, () => {
+export const signupMock = http.post(`${BASE_URL}/signup`, () => {
   const result = { message: 'User Created Successfully', user: existingUser, token: userJwt };
-  return HttpResponse.json(result);
+  return HttpResponse.json(result, {
+    status: 200
+  });
 });
 
-export const authHandler = [signupMock];
+export const signupMockError = http.post(`${BASE_URL}/signup`, () => {
+  const result = { message: 'Invalid Credentials', status: 'BAD_REQUEST', statusCode: 400 };
+  return HttpResponse.json(result, {
+    status: 400
+  });
+});
+
+export const authHandler = [signupMock, signupMockError];
