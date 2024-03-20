898aa935da1796cce04c6ece8c6218f8c5b76f54
Antar <antardas2334@gmail.com>
2024-03-20 15:33:48 +0600

    Added Notification reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import userReducer from '~/redux/reducers/user/user.reducer';
-import userSuggestion from '~/redux/reducers/suggestion/suggestion.reducer';
+import suggestionReducer from '~/redux/reducers/suggestion/suggestion.reducer';
+import notificationReducer from '~/redux/reducers/notification/notification.reducer';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    suggestion: userSuggestion
+    suggestion: suggestionReducer,
+    notification: notificationReducer
   }
 });
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 
 export { store };
