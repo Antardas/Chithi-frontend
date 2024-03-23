5ff2c4dad77f9b3c577ef20c6dbb25287c8b9bbd
Antar <antardas2334@gmail.com>
2024-03-23 15:24:27 +0600

    dispatch the state for socket connection

--- src/services/socket/sokcet.service.ts
+++ src/services/socket/sokcet.service.ts
@@ -1,10 +1,12 @@
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Socket, io } from 'socket.io-client';
+import { setSocketConnected } from '~/redux/reducers/app/app.reducer';
+import { store } from '~/redux/store';
 
 const BASE_URL: string = `${import.meta.env.VITE_BASE_ENDPOINT}`;
 
 class SocketService {
-  private socket!: Socket<DefaultEventsMap, DefaultEventsMap>;
+  public socket!: Socket<DefaultEventsMap, DefaultEventsMap>;
 
   setupSocketConnection() {
     this.socket = io(BASE_URL, {
@@ -18,16 +20,19 @@ class SocketService {
   private socketConnectionEvents() {
     this.socket.on('connect', () => {
       console.log('connected');
+      store.dispatch(setSocketConnected({ socketConnected: true }));
     });
 
     this.socket.on('disconnect', (reason: string) => {
       console.log(`Reason: ${reason}`);
       this.socket.connect();
+      store.dispatch(setSocketConnected({ socketConnected: false }));
     });
 
     this.socket.on('connect_error', (error) => {
       console.log(`Reason: ${error}`);
       this.socket.connect();
+      store.dispatch(setSocketConnected({ socketConnected: false }));
     });
   }
 }
