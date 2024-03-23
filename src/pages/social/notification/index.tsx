d1f4f5d868e68c080a9a6cce7295de1e655bdfae
Antar <antardas2334@gmail.com>
2024-03-23 15:41:22 +0600

    Added Notification page

--- src/pages/social/notification/index.tsx
+++ src/pages/social/notification/index.tsx
@@ -3,13 +3,16 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { FaCircle, FaRegCircle, FaRegTrashAlt } from 'react-icons/fa';
 import Avatar from '~/Components/Avatar';
 import useEffectOnce from '~/hooks/useEffectOnce';
-import { useAppDispatch } from '~/redux/store';
+import { RootState, useAppDispatch } from '~/redux/store';
 import { notificationService } from '~/services/api/notification/notification.service';
 import { Utils } from '~/services/utils/utils.service';
 import { IError } from '~/types/axios';
-import { INotification } from '~/types/notification';
-import '~/pages/social/notification/Notifications.scss'
-
+import { INotification, INotificationPreview } from '~/types/notification';
+import '~/pages/social/notification/Notifications.scss';
+import { NotificationUtils } from '~/services/utils/notification-utils.service';
+import { useSelector } from 'react-redux';
+import { IUser } from '~/types/user';
+import NotificationPreview from '~/Components/Dialog/NotificationPreview';
 const Notifications = () => {
   const [notifications, setNotifications] = useState<INotification[]>([]);
   const [loading, setLoading] = useState(true);
@@ -22,6 +25,7 @@ const Notifications = () => {
   });
   const { profile } = useSelector((state: RootState) => state.user);
   const dispatch = useAppDispatch();
+
   const getUserNotifications = useCallback(async () => {
     setLoading(true);
     try {
@@ -34,10 +38,50 @@ const Notifications = () => {
       }
     }
     setLoading(false);
-  }, []);
+  }, [dispatch]);
+
+  const markAsRead = async (notification: INotification) => {
+    try {
+      const response = await NotificationUtils.markAsRead(notification._id, notification, setNotificationDialogContent);
+      Utils.dispatchNotification(response.data.message, 'success', dispatch);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const typedError: AxiosError<IError> = error;
+        Utils.dispatchNotification(typedError.message, 'error', dispatch);
+      }
+    }
+  };
+
+  const deleteNotification = useCallback(
+    async (notificationId: string) => {
+      setLoading(true);
+      try {
+        const response = await notificationService.deleteNotification(notificationId);
+        Utils.dispatchNotification(response.data.message, 'success', dispatch);
+      } catch (error) {
+        if (isAxiosError(error)) {
+          const typedError: AxiosError<IError> = error;
+          Utils.dispatchNotification(typedError.message, 'error', dispatch);
+        }
+      }
+      setLoading(false);
+    },
+    [dispatch]
+  );
+
+
   useEffectOnce(() => {
     getUserNotifications();
   });
+
+  useEffect(() => {
+    NotificationUtils.socketIONotifications(profile as IUser, notifications, setNotifications, 'notificationPage', () => {});
+    return () => {
+      NotificationUtils.cleanup();
+    };
+  }, [notifications, profile]);
+
+
   return (
     <div>
       {' '}
@@ -57,9 +101,9 @@ const Notifications = () => {
       ) : null}
       <div className="notifications-container">
         <div className="notifications">Notifications</div>
-        <div className="notifications-box">
+        <div className={notifications.length ? 'notifications-box' : ''}>
           {notifications.map((notification, index) => (
-            <div className="notification-box" data-testid="notification-box" key={index}>
+            <div className="notification-box" data-testid="notification-box" key={index} onClick={() => markAsRead(notification)}>
               <div className="notification-box-sub-card">
                 <div className="notification-box-sub-card-media">
                   <div className="notification-box-sub-card-media-image-icon">
@@ -74,7 +118,14 @@ const Notifications = () => {
                   <div className="notification-box-sub-card-media-body">
                     <h6 className="title">
                       {notification?.message}
-                      <small data-testid="subtitle" className="subtitle">
+                      <small
+                        data-testid="subtitle"
+                        className="subtitle"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteNotification(notification._id);
+                        }}
+                      >
                         <FaRegTrashAlt className="trash" />
                       </small>
                     </h6>
