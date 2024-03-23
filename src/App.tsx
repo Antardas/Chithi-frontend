9816b599ea188558632581606a8fb18185ba9ac7
Antar <antardas2334@gmail.com>
2024-03-23 15:45:03 +0600

    Used Socket and Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import '~/App.scss';
 import router from '~/routes';
 import { socketService } from './services/socket/sokcet.service';
-import Toast, { IToastItem } from '~/Components/Toast';
-import checkIcon from '~/assets/images/check.svg';
-import infoIcon from '~/assets/images/info.svg';
-import warningIcon from '~/assets/images/warning.svg';
-import errorIcon from '~/assets/images/error.svg';
+import Toast from '~/Components/Toast';
+// import checkIcon from '~/assets/images/check.svg';
+// import infoIcon from '~/assets/images/info.svg';
+// import warningIcon from '~/assets/images/warning.svg';
+// import errorIcon from '~/assets/images/error.svg';
 import { useSelector } from 'react-redux';
 import { RootState } from './redux/store';
+import useEffectOnce from './hooks/useEffectOnce';
 function App(): JSX.Element {
-  const notifications = useSelector((state:RootState) => state.notification);
-  useEffect(() => {
+  const notifications = useSelector((state: RootState) => state.notification);
+  useEffectOnce(() => {
     socketService.setupSocketConnection();
-  }, []);
+  });
   return (
     <>
-      {notifications && notifications.length? <Toast toastList={notifications} position="top-right" autoDelete={true} /> : null}
+      {notifications && notifications.length ? <Toast toastList={notifications} position="top-right" autoDelete={true} /> : null}
       <RouterProvider router={router} />
     </>
   );
