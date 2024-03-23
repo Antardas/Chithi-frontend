2230b756c3b722c92122aded6d1d05606a397efa
Antar <antardas2334@gmail.com>
2024-03-23 15:31:41 +0600

    Fix the Dropdown types and css scrollbar issue

--- src/Components/Dropdown/index.tsx
+++ src/Components/Dropdown/index.tsx
@@ -1,25 +1,39 @@
+import { CSSProperties } from 'react';
 import Avatar from '~/Components/Avatar';
 import Button from '~/Components/Button';
 import { FaCircle, FaRegCircle, FaTrashAlt, FaUserAlt } from 'react-icons/fa';
 
 import '~/Components/Dropdown/Dropdown.scss';
 import { Utils } from '~/services/utils/utils.service';
-import { CSSProperties } from 'react';
 import { ISettingsDropdown } from '~/types/utils';
+import { INotificationDropdown } from '~/types/notification';
 
-// TODO: here make the dropdown props more dynamic using generic
-export interface DropdownProps {
-  data: ISettingsDropdown[];
-  notificationCount: number;
-  title: string;
-  style: CSSProperties;
-  height: number;
-  onMarkAsRead?: (notification: never) => void; // Optional callback function
-  onDeleteNotification?: (notification: never) => void; // Optional callback function
-  onLogout?: () => void; // Optional callback function
-  onNavigate?: (path: string) => void; // Optional callback function with optional string argument
+type TitleType = 'Notifications' | 'Settings';
+
+interface NotificationProps {
+  title: 'Notifications';
+  onMarkAsRead: (item: INotificationDropdown) => Promise<void>;
+  onDeleteNotification: (id: string) => void;
+  notificationCount: number; // Add notificationCount here
 }
-const Dropdown = ({ data, notificationCount, title, style, height, onMarkAsRead, onDeleteNotification, onLogout, onNavigate }: DropdownProps) => {
+
+interface SettingsProps {
+  title: 'Settings';
+  onLogout: () => void;
+  onNavigate: () => void;
+}
+
+type DropdownProps = ((NotificationProps & { title: 'Notifications' }) | (SettingsProps & { title: 'Settings' })) & {
+  data: ISettingsDropdown[];
+  style?: CSSProperties;
+  height?: number;
+};
+
+
+const Dropdown = ({ data, title, style, height, ...rest }: DropdownProps) => {
+  const { notificationCount, onMarkAsRead, onDeleteNotification } =
+    title === 'Notifications' ? (rest as NotificationProps) : ({} as NotificationProps);
+  const { onLogout, onNavigate } = title === 'Settings' ? (rest as SettingsProps) : ({} as SettingsProps);
   return (
     <div className="social-dropdown" style={style} data-testid="dropdown">
       <div className="social-card">
@@ -35,9 +49,16 @@ const Dropdown = ({ data, notificationCount, title, style, height, onMarkAsRead,
             <div data-testid="info-container" className="social-card-body-info-container" style={{ maxHeight: `${height}px` }}>
               {data.map((item) => (
                 <div className="social-sub-card" key={Utils.generateString(10)}>
+
                   <div className="content-avatar">
                     {title === 'Notifications' ? (
-                      <Avatar name={item?.username} bgColor={item?.avatarColor} textColor="#ffffff" size={40} avatarSrc={item?.profilePicture} />
+                      <Avatar
+                        name={item?.userFrom?.username || ''}
+                        bgColor={item?.userFrom?.avatarColor || ''}
+                        textColor="#ffffff"
+                        size={40}
+                        avatarSrc={item?.userFrom?.profilePicture || ''}
+                      />
                     ) : (
                       <FaUserAlt className="userIcon" />
                     )}
@@ -46,7 +67,7 @@ const Dropdown = ({ data, notificationCount, title, style, height, onMarkAsRead,
                     className="content-body"
                     onClick={() => {
                       if (title === 'Notifications') {
-                        onMarkAsRead(item);
+                        onMarkAsRead(item as INotificationDropdown);
                       } else {
                         onNavigate();
                       }
@@ -57,7 +78,7 @@ const Dropdown = ({ data, notificationCount, title, style, height, onMarkAsRead,
                   </div>
                   {title === 'Notifications' && (
                     <div className="content-icons">
-                      <FaTrashAlt className="trash" onClick={() => onDeleteNotification(item?._id)} />
+                      <FaTrashAlt className="trash" onClick={() => onDeleteNotification(item?._id || '')} />
                       {item?.read ? <FaRegCircle className="circle" /> : <FaCircle className="circle" />}
                     </div>
                   )}
@@ -78,3 +99,16 @@ const Dropdown = ({ data, notificationCount, title, style, height, onMarkAsRead,
 };
 
 export default Dropdown;
+
+// TODO: here make the dropdown props more dynamic using generic
+// export interface DropdownProps {
+//   data: ISettingsDropdown[];
+//   notificationCount: number;
+//   title: string;
+//   style: CSSProperties;
+//   height: number;
+//   onMarkAsRead?: (notification: never) => void; // Optional callback function
+//   onDeleteNotification?: (notification: never) => void; // Optional callback function
+//   onLogout?: () => void; // Optional callback function
+//   onNavigate?: (path: string) => void; // Optional callback function with optional string argument
+// }
