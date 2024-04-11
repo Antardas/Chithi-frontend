4387265487972cb211c5922581d4b8b8df6fd769
Antar <antardas2334@gmail.com>
2024-04-11 13:24:29 +0600

    Added Skeleton

--- src/pages/social/Streams/StreamSkeleton.tsx
+++ src/pages/social/Streams/StreamSkeleton.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PostFormSkeleton from '~/Components/Post/PostForm/PostFormSkeleton';
+import PostSkeleton from '~/Components/Post/PostSkeleton';
 import SuggestionSkeleton from '~/Components/Suggestions/SuggestionSkeleton';
-
+import '~/pages/social/Streams/Streams.scss';
 const StreamSkeleton = () => {
   return (
     <div className="streams" data-testid="streams">
       <div className="streams-content">
         <div className="streams-post">
           <PostFormSkeleton />
-          {/* <PostFormSkeleton />
           {[1, 2, 3, 4, 5, 6].map((index) => (
             <div key={index}>
               <PostSkeleton />
             </div>
-          ))} */}
+          ))}
         </div>
         <div className="streams-suggestions">
           <SuggestionSkeleton />
