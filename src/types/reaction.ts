f543d2b9d12afc72e48bedb704e44b3e897cdfa6
Antar <antardas2334@gmail.com>
2024-04-11 13:11:34 +0600

    Added Reaction Type

--- src/types/reaction.ts
+++ src/types/reaction.ts
@@ -2,3 +2,15 @@ export interface IReaction {
   senderName: string;
   type: string;
 }
+
+export interface IReactionPost {
+  _id?: string;
+  username: string;
+  avatarColor: string;
+  type: string;
+  postId: string;
+  profilePicture: string;
+  createdAt: string;
+  userTo: string;
+  comment: string;
+}
