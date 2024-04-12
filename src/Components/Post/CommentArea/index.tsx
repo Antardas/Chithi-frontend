c82741d7f22fa6655b118abca4017747285976ca
Antar <antardas2334@gmail.com>
2024-04-12 21:23:11 +0600

    Added for reaction checking is user is already added reaction not in  post

--- src/Components/Post/CommentArea/index.tsx
+++ src/Components/Post/CommentArea/index.tsx
@@ -2,27 +2,55 @@
  * This Component is responsible for showing Comment
  */
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FaRegCommentAlt } from 'react-icons/fa';
 import '~/Components/Post/CommentArea/CommentArea.scss';
 import { IPost } from '~/types/post';
 import like from '~/assets/reactions/like.png';
+import Reactions from '~/Components/Post/Reactions';
+import { IReactionPost } from '~/types/reaction';
+import { Utils } from '~/services/utils/utils.service';
+import { reactionsMap } from '~/services/utils/static.data';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/redux/store';
 const CommentArea = ({ post }: ICommentAreaProps) => {
+  const [userSelectedReaction, setUserSelectedReaction] = useState<string>('');
+  // const reactions: IReactionPost[] = [];
+  const { reactions } = useSelector((state: RootState) => state.userReactions);
+  const selectedUserReaction = useCallback(
+    (postReactions: IReactionPost[]) => {
+      const userReaction = postReactions.find((item) => item.postId === post._id);
+      const result = userReaction ? Utils.firstLatterUpperCase(userReaction.type) : '';
+      setUserSelectedReaction(result);
+    },
+    [post]
+  );
+
+  const addReactionToPost = async (reaction: string): Promise<void> => {};
+
+  useEffect(() => {
+    selectedUserReaction(reactions);
+  }, [selectedUserReaction, reactions]);
   return (
     <div className="comment-area" data-testid="comment-area">
       <div className="like-icon reactions">
         <div className="likes-block">
-          <div className="like likes-block-icons reaction-icon">
-            <div className="reaction-display like" data-testid="selected-reaction">
-              <img className="reaction-img" src={like} alt="" />
-              <span>Like</span>
-            </div>
-            {/* <div className="reaction-display" data-testid="default-reaction">
-                    <img className="reaction-img" src="" alt="" /> <span>Like</span>
-                  </div> */}
+          <div className={`likes-block-icons reaction-icon ${userSelectedReaction.toLowerCase()}`}>
+            {userSelectedReaction ? (
+              <div className={`reaction-display ${userSelectedReaction.toLowerCase()}`} data-testid="selected-reaction">
+                <img className="reaction-img" src={reactionsMap[userSelectedReaction.toLowerCase()]} alt="" />
+                <span>{userSelectedReaction}</span>
+              </div>
+            ) : (
+              <div className="reaction-display" data-testid="default-reaction">
+                <img className="reaction-img" src={like} alt="" /> <span>Like</span>
+              </div>
+            )}
           </div>
         </div>
-        <div className="reactions-container app-reactions">Reactions</div>
+        <div className="reactions-container app-reactions">
+          <Reactions onClick={addReactionToPost} />
+        </div>
       </div>
       <div className="comment-block">
         <span className="comments-text">
