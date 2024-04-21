a1b6498ad104283a42fa0ee3e4bb23ae39cabdb9
Antar Das <antardas2334@gmail.com>
2024-04-21 23:16:42 +0600

    get all followings

--- src/Components/Posts/index.tsx
+++ src/Components/Posts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import '~/Components/Posts/Posts.scss';
 import { RootState } from '~/redux/store';
@@ -8,9 +8,11 @@ import { Utils } from '~/services/utils/utils.service';
 import { PostUtils } from '~/services/utils/post-utils.service';
 import { IUser } from '~/types/user';
 import PostSkeleton from '../Post/PostSkeleton';
+import { IFollower } from '~/types/follower';
+import { id } from 'date-fns/locale';
 export interface IPostsProps {
   posts: IPost[];
-  userFollowing: unknown[];
+  userFollowing: IFollower[];
   postsLoading?: boolean;
 }
 const Posts = ({ posts, userFollowing, postsLoading }: IPostsProps) => {
@@ -18,12 +20,17 @@ const Posts = ({ posts, userFollowing, postsLoading }: IPostsProps) => {
   const [followings, setFollowings] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   // later I will use the Profile, userFollowing, postsLoading
+  useEffect(() => {
+    setFollowings(() => {
+      return userFollowing.map((follower) => follower._id);
+    });
+  }, [userFollowing]);
   return (
     <div className="post-container" data-testid="posts">
       {postsLoading ? <PostsSkeleton /> : null}
       {posts.map((post) => (
         <div key={`post-container-${post._id}`} data-testid="posts-item">
-          {!Utils.checkIfUserIsBlocked(profile?.blockedBy as string[], post.userId)|| post.userId === profile?._id  ? (
+          {!Utils.checkIfUserIsBlocked(profile?.blockedBy as string[], post.userId) || post.userId === profile?._id ? (
             PostUtils.checkPrivacy(post, profile as IUser, followings) ? (
               <Post post={post} showIcons={false} loading={loading} key={`post-${post._id}`} />
             ) : null
