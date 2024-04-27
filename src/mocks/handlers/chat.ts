f83c030f8d1a0fc403fa05256db34faf8f58b17d
Antar Das <antardas2334@gmail.com>
2024-04-27 19:08:47 +0600

    Fix Chat Handler

--- src/mocks/handlers/chat.ts
+++ src/mocks/handlers/chat.ts
@@ -1,38 +1,30 @@
-import { messageData } from '@mocks/data/chat.mock';
-import { BASE_ENDPOINT } from '@services/axios';
-import { rest } from 'msw';
+import { HttpResponse, http } from 'msw';
+import { BASE_URL } from '~/services/axios';
+import { messageData } from '../data/chat.mock';
 
-const BASE_URL = `${BASE_ENDPOINT}/api/v1`;
-
-export const emptyChatListMock = rest.get(`${BASE_URL}/chat/message/conversation-list`, (req, res, ctx) => {
-  const result = { message: 'User conversation list', list: [] };
-  return res(ctx.status(200), ctx.json(result));
+export const emptyChatListMock = http.get(`${BASE_URL}/chat/message/conversation`, () => {
+  const result = { message: 'User conversation list', data: [] };
+  return HttpResponse.json(result, { status: 200 });
 });
 
-export const chatListMock = rest.get(`${BASE_URL}/chat/message/conversation-list`, (req, res, ctx) => {
-  const result = { message: 'User conversation list', list: [messageData] };
-  return res(ctx.status(200), ctx.json(result));
+export const chatListMock = http.get(`${BASE_URL}/chat/message/conversation`, () => {
+  const result = { message: 'User conversation list', data: [messageData] };
+  return HttpResponse.json(result, { status: 200 });
 });
 
-export const chatMessagesMock = rest.get(`${BASE_URL}/chat/message/user/123456`, (req, res, ctx) => {
+export const chatMessagesMock = http.get(`${BASE_URL}/chat/message/user/123456`, () => {
   const result = { message: 'User chat messages', messages: [messageData, messageData] };
-  return res(ctx.status(200), ctx.json(result));
+  return HttpResponse.json(result, { status: 200 });
 });
 
-export const chatMessagesUserTwoMock = rest.get(`${BASE_URL}/chat/message/user/222222`, (req, res, ctx) => {
+export const chatMessagesUserTwoMock = http.get(`${BASE_URL}/chat/message/user/222222`, () => {
   const result = { message: 'User chat messages', messages: [messageData, messageData] };
-  return res(ctx.status(200), ctx.json(result));
+  return HttpResponse.json(result, { status: 200 });
 });
 
-export const addChatUsersMock = rest.post(`${BASE_URL}/chat/message/add-chat-users`, (req, res, ctx) => {
+export const addChatUsersMock = http.post(`${BASE_URL}/chat/message/add-chat-users`, () => {
   const result = { message: 'Users added' };
-  return res(ctx.status(200), ctx.json(result));
+  return HttpResponse.json(result, { status: 200 });
 });
 
-export const chatHandlers = [
-  emptyChatListMock,
-  chatListMock,
-  chatMessagesMock,
-  addChatUsersMock,
-  chatMessagesUserTwoMock
-];
+export const chatHandlers = [emptyChatListMock, chatListMock, chatMessagesMock, addChatUsersMock, chatMessagesUserTwoMock];
