af7c13a88e489a37fa63b9ce74aad3766b315a0c
Antar <antardas2334@gmail.com>
2024-05-01 23:55:30 +0600

    Added Profile  Background Cover and Profile Picture Component

--- src/Components/BackgroundHeader/index.tsx
+++ src/Components/BackgroundHeader/index.tsx
@@ -6,7 +6,10 @@ import Input from '../Input';
 import { FaCamera } from 'react-icons/fa';
 import { IUser } from '~/types/user';
 import ImageGrid from '../ImageGrid';
-
+import { ITabItems } from '~/types/utils';
+import '~/Components/BackgroundHeader/BackgroundHeader.scss';
+import BackgroundHeaderSkeleton from './BackgroundHeaderSkeleton';
+import { IImageData } from '~/types/image';
 const BackgroundHeader = ({
   user,
   loading,
@@ -35,6 +38,8 @@ const BackgroundHeader = ({
       backgroundFileRef.current.click();
     }
   };
+
+
 
   const profileFileInputClicked = () => {
     if (profileImageRef.current) {
@@ -88,90 +93,168 @@ const BackgroundHeader = ({
         <ImageGrid
           images={galleryImages}
           closeModal={() => setShowImagesModal(false)}
-          selectedImage={(event) => {
-            setSelectedBackground(event);
-            selectedFileImage(event, 'background');
+          selectedImage={(url) => {
+            setSelectedBackground(url);
+            selectedFileImage(url, 'background');
           }}
         />
       )}
 
-      <div className="profile-banner" data-testid="profile-banner">
-        {hasImage ? (
-          <div className="save-changes-container" data-testid="save-changes-container">
-            <div className="save-changes-box">
-              <div className="spinner-container">{showSpinner && !hasError ? <Spinner bgColor="white" /> : null}</div>
-              <div className="save-changes-buttons">
-                <div className="save-changes-buttons-bg">
-                  <Button
-                    label="Cancel"
-                    className="cancel change-btn"
-                    disabled={false}
-                    onClick={() => {
-                      setShowSpinner(false);
-                      cancelFileSelection();
-                      hideSaveChangesContainer();
-                    }}
-                  />
-                  <Button
-                    label="Save Changes"
-                    className="save change-btn"
-                    disabled={false}
-                    onClick={() => {
-                      setShowSpinner(true);
-                      const type = selectedBackground ? 'background' : 'profile';
-                      saveImage(type);
-                    }}
-                  />
+      {loading ? (
+        <BackgroundHeaderSkeleton tabItems={tabItems} />
+      ) : (
+        <div className="profile-banner" data-testid="profile-banner">
+          {hasImage ? (
+            <div className="save-changes-container" data-testid="save-changes-container">
+              <div className="save-changes-box">
+                <div className="spinner-container">{showSpinner && !hasError ? <Spinner bgColor="white" /> : null}</div>
+                <div className="save-changes-buttons">
+                  <div className="save-changes-buttons-bg">
+                    <Button
+                      label="Cancel"
+                      className="cancel change-btn"
+                      disabled={false}
+                      onClick={() => {
+                        setShowSpinner(false);
+                        cancelFileSelection();
+                        hideSaveChangesContainer();
+                      }}
+                    />
+                    <Button
+                      label="Save Changes"
+                      className="save change-btn"
+                      disabled={false}
+                      onClick={() => {
+                        setShowSpinner(true);
+                        const type = selectedBackground ? 'background' : 'profile';
+                        saveImage(type);
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+          ) : null}
+          <div
+            data-testid="profile-banner-image"
+            className="profile-banner-image"
+            style={{
+              background: `${!selectedBackground ? user.avatarColor : ''}`
+            }}
+          >
+            {url && hideSettings && !hasImage ? (
+              <div className="delete-btn" data-testid="delete-btn">
+                <Button
+                  label="Remove"
+                  className="remove"
+                  disabled={false}
+                  onClick={() => {
+                    removeBackgroundImage(user.bgImageId, user.bgImageVersion);
+                  }}
+                />
+              </div>
+            ) : null}
+            {!selectedBackground && !url ? <h3>Add a background image</h3> : <img src={`${selectedBackground || url}`} alt="" />}
           </div>
-        ) : null}
-        <div data-testid="profile-banner-image" className="profile-banner-image">
-          <div className="delete-btn" data-testid="delete-btn">
-            <Button label="Remove" className="remove" disabled={false} />
-          </div>
-          <h3>Add a background image</h3>
-          <img src="" alt="" />
-        </div>
-        <div className="profile-banner-data">
-          <div data-testid="profile-pic" className="profile-pic">
-            <Avatar
-              name={user?.username as string}
-              bgColor={user?.avatarColor}
-              textColor="#ffffff"
-              size={180}
-              avatarSrc={selectedProfileImage || user?.profilePicture}
-            />
-            <div className="profile-pic-select" data-testid="profile-pic-select">
-              <Input type="file" className="inputFile" />
-              <label>
-                <FaCamera className="camera" />
-              </label>
-            </div>
-          </div>
-          <div className="profile-name">Danny</div>
-          <div className="profile-select-image">
-            <Input type="file" className="inputFile" />
-            <label data-testid="add-cover-photo">
-              <FaCamera className="camera" /> <span>Add Cover Photo</span>
-            </label>
-            Background select dropdown
-          </div>
-        </div>
-        <div className="profile-banner-items">
-          <ul className="banner-nav">
-            <div data-testid="tab-elements">
-              <li className="banner-nav-item">
-                <div className="banner-nav-item-name">
-                  Icon
-                  <p className="title">Item</p>
+          <div className="profile-banner-data">
+            <div
+              data-testid="profile-pic"
+              className="profile-pic"
+              style={{
+                width: `${user.profilePicture ? '180px' : ''}`
+              }}
+            >
+              <Avatar
+                name={user?.username as string}
+                bgColor={user?.avatarColor}
+                textColor="#ffffff"
+                size={180}
+                avatarSrc={selectedProfileImage || user?.profilePicture}
+              />
+
+              {hideSettings ? (
+                <div className="profile-pic-select" data-testid="profile-pic-select">
+                  <Input
+                    type="file"
+                    className="inputFile"
+                    name=""
+                    id=""
+                    ref={profileImageRef}
+                    onClick={(event) => {
+                      if (profileImageRef.current) {
+                        profileImageRef.current.value = '';
+                      }
+                    }}
+                    onChange={(event) => {
+                      if (event.target.files && event.target.files.length) {
+                        setSelectedProfileImage(URL.createObjectURL(event.target.files[0]));
+                        selectedFileImage(event.target.files[0], 'profile');
+                      }
+                    }}
+                  />
+                  <label
+                    onClick={() => {
+                      profileFileInputClicked();
+                    }}
+                  >
+                    <FaCamera className="camera" />
+                  </label>
                 </div>
-              </li>
+              ) : null}
             </div>
-          </ul>
+            <div className="profile-name">{user.username}</div>
+            {hideSettings ? (
+              <div className="profile-select-image">
+                <Input
+                  type="file"
+                  className="inputFile"
+                  id=""
+                  name=""
+                  ref={backgroundFileRef}
+                  onClick={(event) => {
+                    if (backgroundFileRef.current) {
+                      backgroundFileRef.current.value = '';
+                    }
+                  }}
+                  onChange={(event) => {
+                    if (event.target.files && event.target.files.length) {
+                      setSelectedBackground(URL.createObjectURL(event.target.files[0]));
+                      selectedFileImage(event.target.files[0], 'background');
+                    }
+                  }}
+                />
+                <label data-testid="add-cover-photo" onClick={() => onAddProfileClick()}>
+                  <FaCamera className="camera" /> <span>Add Cover Photo</span>
+                </label>
+                {isActive ? <BackgroundSelectDropdown /> : null}
+              </div>
+            ) : null}
+          </div>
+          <div className="profile-banner-items">
+            <ul className="banner-nav">
+              {tabItems.map((item: ITabItems) => {
+                return (
+                  <div data-testid="tab-elements" key={item.key}>
+                    {item.show ? (
+                      <li className="banner-nav-item">
+                        <div
+                          className={`banner-nav-item-name ${tab.toLowerCase() === item.key.toLowerCase() ? 'active' : ''}`}
+                          onClick={() => {
+                            onClick(item.key);
+                          }}
+                        >
+                          {item.icon}
+                          <p className="title">{item.key}</p>
+                        </div>
+                      </li>
+                    ) : null}
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
@@ -182,26 +265,15 @@ interface BackgroundHeaderProps {
   user: IUser;
   loading: boolean;
   url: string;
-  onClick: () => void;
+  onClick: (tab: string) => void;
   tab: string;
   hasImage: boolean;
-  tabItems: [
-    {
-      key: string;
-      show: boolean;
-      icon: JSX.Element;
-    }
-  ];
+  tabItems: ITabItems[];
   hasError: boolean;
   hideSettings: boolean;
-  selectedFileImage: (url: string, type: string) => void;
+  selectedFileImage: (url: string | File, type: string) => void;
   saveImage: (type: 'background' | 'profile') => void;
   cancelFileSelection: () => void;
-  removeBackgroundImage: () => void;
-  galleryImages: [
-    {
-      imgId: string;
-      imgVersion: string;
-    }
-  ];
+  removeBackgroundImage: (imgId: string, imgVersion: string) => void;
+  galleryImages: IImageData[];
 }
--- src/pages/social/profiles/index.tsx
+++ src/pages/social/profiles/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams, useSearchParams } from 'react-router-dom';
 import BackgroundHeader from '~/Components/BackgroundHeader';
@@ -7,7 +7,11 @@ import { userService } from '~/services/api/user/user.service';
 import { tabItems } from '~/services/utils/static.data';
 import { Utils } from '~/services/utils/utils.service';
 import { IUser } from '~/types/user';
-import '~/pages/social/profiles/Profiles.scss'
+import '~/pages/social/profiles/Profiles.scss';
+import { ITabItems } from '~/types/utils';
+import { ImageUtils } from '~/services/utils/image-utils.service';
+import { imageService } from '~/services/api/image/image.service';
+import { IImageData } from '~/types/image';
 const Profiles = () => {
   const { profile } = useSelector((state: RootState) => state.user);
   const { deleteDialogIsOpen, data } = useSelector((state: RootState) => state.modal);
@@ -15,28 +19,22 @@ const Profiles = () => {
   const [rendered, setRendered] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [hasImage, setHasImage] = useState(false);
-  const [selectedBackgroundImage, setSelectedBackgroundImage] = useState('');
+  const [selectedBackgroundImage, setSelectedBackgroundImage] = useState<string>('');
   const [selectedProfileImage, setSelectedProfileImage] = useState('');
   const [bgUrl, setBgUrl] = useState('');
-  const [galleryImages, setGalleryImages] = useState<
-    [
-      {
-        imgId: string;
-        imgVersion: string;
-      }
-    ]
-  >([]);
+  const [galleryImages, setGalleryImages] = useState<IImageData[]>([]);
   const [imageUrl, setImageUrl] = useState('');
   const [displayContent, setDisplayContent] = useState('timeline');
   const [loading, setLoading] = useState(true);
   const [showImageModal, setShowImageModal] = useState(false);
-  const [userProfileData, setUserProfileData] = useState(null);
+  const [userProfileData, setUserProfileData] = useState<IUser | null>(null);
+  const isRendered = useRef(false);
   const dispatch = useAppDispatch();
   const { username } = useParams();
   const [searchParams] = useSearchParams();
 
   const getUserProfileByUsername = useCallback(async () => {
-    const id = searchParams.get('uId'),
+    const id = searchParams.get('id'),
       uId = searchParams.get('uId');
     if (!username || !id || !uId) return;
     try {
@@ -48,23 +46,106 @@ const Profiles = () => {
       const newUser = response.data.data.user;
       setUser(newUser);
       // setUserProfileData(response.data);
-      setBgUrl(Utils.generateImageUrl(newUser.bgImageVersion, newUser.bgImageId));
+      if (newUser?.bgImageVersion && newUser?.bgImageVersion) {
+        setBgUrl(Utils.generateImageUrl(newUser.bgImageVersion, newUser.bgImageId));
+      }
       setLoading(false);
     } catch (error) {
       Utils.addErrorNotification(error, dispatch);
     }
   }, [dispatch, searchParams, username]);
 
-  const changeTabContent = () => {};
-  const selectedFileImage = () => {};
-  const saveImage = () => {};
-  const cancelFileSelection = () => {};
-  const removeBackgroundImage = () => {};
+  const getUserImages = useCallback(async () => {
+    const id = searchParams.get('id'),
+      uId = searchParams.get('uId');
+    if (!username || !id || !uId) return;
+    try {
+      const response = await imageService.getUserImages(id);
+      setGalleryImages(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      Utils.addErrorNotification(error, dispatch);
+    }
+  }, [dispatch, searchParams, username]);
+
+  const changeTabContent = (data: string) => {
+    setDisplayContent(data);
+  };
+
+  const selectedFileImage = async (data: string | File, type: string) => {
+    setHasImage(!hasImage);
+    if (data instanceof File) {
+      if (type === 'background') {
+        const isValidFile: boolean = ImageUtils.checkFile(data);
+
+        if (!isValidFile) {
+          return;
+        }
+        // const renamedFIle = ImageUtils.renameImage(data);
+        // console.log(renamedFIle);
+
+        const base64 = await ImageUtils.readAsBase64(data);
+        setSelectedBackgroundImage(base64 as string);
+      } else {
+        const isValidFile: boolean = ImageUtils.checkFile(data);
+
+        if (!isValidFile) {
+          return;
+        }
+        const base64 = await ImageUtils.readAsBase64(data);
+        setSelectedProfileImage(base64 as string);
+      }
+    } else {
+      if (type === 'background') {
+        setSelectedBackgroundImage(data as string);
+      } else {
+        setSelectedProfileImage(data as string);
+      }
+    }
+  };
+
+  const saveImage = async (type: string) => {
+    // setLoading(true);
+    try {
+      if (type === 'background') {
+        await imageService.addUserBackgroundImage(selectedBackgroundImage);
+      } else {
+        await imageService.addUserProfileImage(selectedProfileImage);
+      }
+      // setLoading(false);
+      setHasError(false);
+      setHasImage(false);
+    } catch (error) {
+      // setLoading(false);
+      setHasError(true);
+      Utils.addErrorNotification(error, dispatch);
+    }
+  };
+  const cancelFileSelection = () => {
+    setHasImage(false);
+    setSelectedBackgroundImage('');
+    setSelectedProfileImage('');
+    setHasError(false);
+  };
+  const removeBackgroundImage = async (imgId: string, imgVersion: string) => {
+    try {
+      await imageService.deleteUserBackgroundImage(imgId);
+      setBgUrl('');
+    } catch (error) {
+      setHasError(true);
+      Utils.addErrorNotification(error, dispatch);
+    }
+  };
   // const tabItems = () => {};
 
   useEffect(() => {
-    getUserProfileByUsername();
-  }, [])
+    if (!isRendered.current) {
+      getUserProfileByUsername();
+      getUserImages();
+    } else {
+      isRendered.current = true;
+    }
+  }, [getUserProfileByUsername, getUserImages]);
   return (
     <>
       <div className="profile-wrapper">
