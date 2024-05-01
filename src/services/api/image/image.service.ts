1c52c65f0f19354b923bf9095896325d1f67fc6f
Antar <antardas2334@gmail.com>
2024-05-01 23:54:11 +0600

    Added Image Service

---
+++ src/services/api/image/image.service.ts
@@ -0,0 +1,33 @@
+import { AxiosResponse } from 'axios';
+import axios from '~/services/axios';
+import { OnlyMessageResponse } from '~/types/axios';
+import { IAddImage, IGetImages, IImageData } from '~/types/image';
+
+class ImageService {
+  async addUserProfileImage(imageBase64Str: string): Promise<AxiosResponse<IAddImage>> {
+    const response = await axios.post<IAddImage>('/images/profile', {
+      image: imageBase64Str
+    });
+    return response;
+  }
+  async addUserBackgroundImage(imageBase64Str: string): Promise<AxiosResponse<OnlyMessageResponse>> {
+    const response = await axios.post<OnlyMessageResponse>('/images/background', {
+      image: imageBase64Str
+    });
+    return response;
+  }
+  async getUserImages(userId: string): Promise<AxiosResponse<IGetImages>> {
+    const response = await axios.get<IGetImages>(`/images/${userId}`);
+    return response;
+  }
+  async deleteUserProfileImage(imageId: string): Promise<AxiosResponse<OnlyMessageResponse>> {
+    const response = await axios.delete<OnlyMessageResponse>(`/images/profile/${imageId}`);
+    return response;
+  }
+  async deleteUserBackgroundImage(imageId: string): Promise<AxiosResponse<OnlyMessageResponse>> {
+    const response = await axios.delete<OnlyMessageResponse>(`/images/background/${imageId}`);
+    return response;
+  }
+}
+
+export const imageService: ImageService = new ImageService();
