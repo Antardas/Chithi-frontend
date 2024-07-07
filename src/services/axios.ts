cb92fd9c10254b2c0f0817073dc2a696615d14e2
Antar <antardas2334@gmail.com>
2024-07-07 11:57:09 +0600

    used Axios interceptor

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,16 +2,25 @@ import axios from 'axios';
 
 export const BASE_URL: string = `${import.meta.env.VITE_BASE_ENDPOINT}/api/v1`;
 
-export default axios
-  .create({
-    baseURL: BASE_URL,
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    withCredentials: true
-  })
-  .interceptors.request.use((config) => {
-    config.headers['Authorization'] = window.localStorage.getItem('token') || 'TestToken';
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  },
+  withCredentials: true
+});
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    axios.defaults.headers['Authorization'] = localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
     return config;
-  });
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
