1dacfb13afb8d523529cbb7e2f858d6f012b2ca7
Antar <antardas2334@gmail.com>
2024-07-14 13:05:37 +0600

    fix: show PostForm even if post length is 0 and refactor code

--- src/Components/Timeline/index.tsx
+++ src/Components/Timeline/index.tsx
@@ -139,41 +139,6 @@ const Timeline = ({ loading, userProfileData, posts: userPosts }: Props) => {
             />
           </div>
         </div>
-        {/* {loading && !posts.length && (
-          <div className="timeline-wrapper-container-main">
-            <div style={{ marginBottom: '10px' }}>
-              <PostFormSkeleton />
-            </div>
-            <>
-              {[1, 2, 3, 4, 5].map((index) => (
-                <div key={index}>
-                  <PostSkeleton />
-                </div>
-              ))}
-            </>
-          </div>
-        )}
-        {!loading && posts.length > 0 && (
-          <div className="timeline-wrapper-container-main">
-            {username === profile?.username && <PostForm />}
-            {posts.map((post) => (
-              <div key={`post-container-${post._id}`} data-testid="posts-item">
-                {!Utils.checkIfUserIsBlocked(profile?.blockedBy as string[], post.userId) || post.userId === profile?._id ? (
-                  PostUtils.checkPrivacy(post, profile as IUser, following) ? (
-                    <Post post={post} showIcons={username === profile?.username} loading={loading} key={`post-${post._id}`} />
-                  ) : null
-                ) : null}
-              </div>
-            ))}
-          </div>
-        )}
-        {!loading && posts.length === 0 && (
-          <div className="timeline-wrapper-container-main">
-            <div className="empty-page" data-testid="empty-page">
-              No post available
-            </div>
-          </div>
-        )} */}
         {profile && username ? <TimelineWrapper following={following} loading={loading} posts={posts} profile={profile} username={username} /> : null}
       </div>
     </div>
